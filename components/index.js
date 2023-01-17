43347ce08f95169f135b8ff35ce867259f9ca853
gantalaavinash <gantalaavinash@gmail.com>
2023-01-17 16:44:18 +0530

    changes on material page and added goals of association

--- components/index.js
+++ components/index.js
@@ -3,7 +3,7 @@ import { TypingText, TitleText } from './CustomTexts';
 import PlacementCard from './PlacementCard';
 import Academic from './Academic';
 import EventCard from './EventCard';
-import GoalsOfCS from './GoalsOfCS'
+import GoalsOfCS from './GoalsOfCS';
 import Footer from './Footer';
 import StartSteps from './StartSteps';
 
